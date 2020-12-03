c5c88e196eaa486128887481227da58746405c89
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2020-12-02 16:51:55 -0800

    use safepaths

--- reporting/attendee-from-outlook.R
+++ reporting/attendee-from-outlook.R
@@ -10,9 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-
-
-
 library(readxl)
 library(dplyr)
 library(ggplot2)
